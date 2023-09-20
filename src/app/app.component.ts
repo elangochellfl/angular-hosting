dd1958037e7e537867b23db2757c407c14aed0a1
elango <elangochell@gmail.com>
2023-09-20 19:33:39 +0530

    reverseTex0t Method updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { splitAtColon } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,12 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-hosting-project';
-
+  href: string = "https://angular.io/tutorial";
   message: string = "I am in Guduvanchery";
   message2: string = "CLI Documentation from component - updated";
   myStyles = {
     'color': '#FFFFFF',
     'background-color': 'blue',
     'font-style': 'italic'
+  }
+
+  ngOnInit(): void {
+    // setTimeout (() => { 
+    //   this.reverseText();
+    // }, 1000);
+    
+  }
+  reverseText() {
+    let text: string = "hi this is my world!!!";
+    //this.message = text.split(' ').reverse().join(' ');
+    this.message = text.split(' ').reverse().join(' ').split('').reverse().join('');
+    document.write(this.message);
   }
 }
